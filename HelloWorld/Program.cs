68f2b4cc560e4f90d03892b804f0ef79cae77e5e
Martin Šalata <martin.salata@outlook.com>
2025-04-10 19:06:08 +0200

    Remove unnecessary line

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Preparing for writing hello world!");
-
         Console.WriteLine("Hello, World! 😎");
     }
 }
